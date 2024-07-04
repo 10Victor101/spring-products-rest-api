31e0d6eb3586a557d1463de1145058a154007e7f
victor.damasceno <victordamascenos2015@gmail.com>
2024-07-04 20:03:42 -0300

    feat: ProductRepository

---
+++ src/main/java/com/example/products_api/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.products_api.repositories;
+
+
+import com.example.products_api.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
